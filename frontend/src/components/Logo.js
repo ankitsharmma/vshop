3fd3d58666c342804d3acf444b0a64cee669359a
Ankit sharma <sharmaankit7090@gmail.com>
2024-11-22 14:05:59 +0530

    first commit

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Logo = ({w,h}) => {
+ 
+
+return (
+    <>
+
+    <h6 style={{color:'red', fontSize:'1.1rem',fontFamily:"sans-serif", fontWeight:"bolder"}}>V-shop</h6>
+    </>
+
+)
+}
+
+export default Logo
